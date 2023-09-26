d03e8f5a3d961441ebacaa829bb66adb75955981
gabrielgiroe1 <gabriel.giroe@gmail.com>
2023-09-26 14:07:19 +0300

    Solved error

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'pages/home'
+  get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
